1f252ff16c65ba5db3986247ba6a85b66141ebc3
Mona Abdollahi <abd.mona@hotmail.com>
2017-05-29 22:01:31 +0200

    add function in myForm to save input

--- my-react-app-test/src/myForm.js
+++ my-react-app-test/src/myForm.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 
 
 class MyForm extends Component {
- render() {
-    return <div>
-        <input placeholder="name" />
-        <input placeholder="email" />
-        </div>;
+  constructor(props) {
+      super(props);
+
+      this.saveName= this.saveName.bind(this);
+     this.saveEmail= this.saveEmail.bind(this);
   }
+ render() {
+    return (<div>
+    <input onChange={this.saveName} placeholder="name" />
+    <input onChange={this.saveEmail} placeholder="email" />
+        </div>
+    );
+  }
+  saveName (event) {
+        let name= event.target.value;
+        this.setState ({
+            name: name
+       });
+         console.log(name);
+
+    }
+     saveEmail (event) {
+        let email= event.target.value;
+        this.setState ({
+            email: email
+        });
+         console.log(email);
+   }
 }
 
 export default MyForm;
